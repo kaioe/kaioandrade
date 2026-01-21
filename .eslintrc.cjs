a729ed23f31447424b043da5d97b7bd7c5418af7
Kaio Andrade <kaioed@gmail.com>
2026-01-21 10:19:09 +1000

    feat: add TypeScript ESLint support and update project assets
    - Add @typescript-eslint/eslint-plugin and parser to dependencies
    - Configure ESLint with TypeScript rules, disable conflicting JS rules, and set React version detection
    - Update AAFS Gold Coast project HTML to use local logo image instead of external URL, and add inline script for the application build
    
    This enhances code quality with TypeScript linting and localizes assets for better performance and portability.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,9 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
@@ -13,5 +14,14 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off', // Allow unused vars for development
+    'no-undef': 'off', // TypeScript handles this
+    '@typescript-eslint/no-require-imports': 'off', // Allow require in config files
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
